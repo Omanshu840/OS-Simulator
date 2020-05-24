4a42a66f7fe89b9fa9b64ea04fbab649a0306ce3
Niharrai <50584781+Niharrai@users.noreply.github.com>
2020-05-24 07:38:50 +0530

    memalloc/

--- memalloc/urls.py
+++ memalloc/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name="memalloc"
+
 urlpatterns = [
     path('', views.index, name='index'),
 ]
--- memalloc/views.py
+++ memalloc/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Memory Allocation home page")
+    return render(request,
+    				template_name = 'memalloc/MFT.html')
