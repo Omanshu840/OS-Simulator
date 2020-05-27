732e87c1e8be5546a63c95bd175e519ae1399e57
Niharrai <50584781+Niharrai@users.noreply.github.com>
2020-05-27 17:20:56 +0530

    memalloc/

--- memalloc/views.py
+++ memalloc/views.py
@@ -2,6 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
+def mftindex(request):
     return render(request,
     				template_name = 'memalloc/MFT.html')
+def mvtindex(request):
+	return render(request,
+					template_name = 'memalloc/mvt.html')
+def mvtbf(request):
+	return render(request,
+					template_name = 'memalloc/mvt/bf/bf.html')
+
+def mvtff(request):
+	return render(request,
+					template_name = 'memalloc/mvt/ff/ff.html')
+
+def mvtwf(request):
+	return render(request,
+					template_name = 'memalloc/mvt/wf/wf.html')
+	
+	
